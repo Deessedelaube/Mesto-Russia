514210c65f580e03e7b0166dfb79a809be8abca9
Daria Mishunina <mishuninadaria@gmail.com>
2020-09-12 21:51:25 +0400

    feat structure

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,62 @@
-const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup_profile');
+const popupAddElement = document.querySelector('.popup_addElement');
 const page = document.querySelector('.page');
-const popupOpenButton = document.querySelector('.button_type_edit');
-const popupClose = popup.querySelector('.button_type_close');
-// Находим форму в DOM
-let formElement = popup.querySelector('.form');
-// Находим поля формы в DOM
-let nameInput = popup.querySelector('.form__fullname');
-let jobInput = popup.querySelector('.form__description');
-// Выбираем элементы, куда должны быть вставлены значения полей
+const popupProfileOpenButton = document.querySelector('.button_type_edit');
+const popupAddElementOpenButton = document.querySelector('.button_type_add');
+// Можно ли найти универсальную кнопку закрытия?
+const popupProfileClose = popupProfile.querySelector('.button_type_close');
+const popupAddElementClose = popupAddElement.querySelector('.button_type_close');
+
+// Находим форму редактирования профиля и ее поля в DOM
+let formProfile = popupProfile.querySelector('.form_profile');
+let nameInput = popupProfile.querySelector('.form__fullname');
+let jobInput = popupProfile.querySelector('.form__description');
+// Выбираем элементы профиля, куда должны быть вставлены значения полей
 let profileName = document.querySelector('.profile__name');
 let profilejob = document.querySelector('.profile__job');
 
-//* функция открытия/закрытия поп-апа
-const popupToggle = () => {
+let formAddElement = popupAddElement.querySelector('form_addElement');
+let titleInput = popupAddElement.querySelector('.form__fullname');
+let srcInput = popupAddElement.querySelector('.form__description');
 
+//функция открытия/закрытия попапа добавления карточки
+const popupToggle = () =>{
+  popupAddElement.classList.toggle('popup_opened');
+  page.classList.toggle('page_overflow_hidden');
+};
+
+//* функция открытия/закрытия попапа профиля
+const popupProfileToggle = () => {
   //если форма была закрыта, то записываем в форму значения полей профиля
-if (popup.classList.contains('popup_opened') === false){
-  nameInput.textContent = profileName.value;
-  jobInput.textContent = profilejob.value;
-}
-//открываем-закрываем попап
-  popup.classList.toggle('popup_opened');
+  if (!popupProfile.classList.contains('popup_opened')){
+    nameInput.value = profileName.textContent;
+    jobInput.value = profilejob.textContent;
+  }
+  //открываем-закрываем попап
+  popupProfile.classList.toggle('popup_opened');
   page.classList.toggle('page_overflow_hidden');
 };
 
 //* функция закрытия по клику не на форму
 const popupCloseByClickonOverlay = (event) => {
-  // console.log({target: event.target,
-  // currentTarget: event.currentTarget,});
   if (event.target === event.currentTarget){
-    popupToggle();
+    popupProfileToggle();
   } else {return}};
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
+// Обработчик «отправки» формы
+function formProfileSubmitHandler (evt) {
   evt.preventDefault(); //убираем стандартную отправку
-
 // Вставляем новые значения с помощью textContent
-profileName.textContent = nameInput.value;
-profilejob.textContent = jobInput.value;
-popupToggle();
+  profileName.textContent = nameInput.value;
+  profilejob.textContent = jobInput.value;
+  popupProfileToggle();
 }
 
 //Вставляем все слушатели событий
-popupOpenButton.addEventListener('click', popupToggle);
-formElement.addEventListener('submit', formSubmitHandler);
-popupClose.addEventListener('click', popupToggle);
-popup.addEventListener('click', popupCloseByClickonOverlay);
+popupProfileOpenButton.addEventListener('click', popupProfileToggle);
+formProfile.addEventListener('submit', formProfileSubmitHandler);
+popupProfileClose.addEventListener('click', popupProfileToggle);
+popupProfile.addEventListener('click', popupCloseByClickonOverlay);
+
+popupAddElementOpenButton.addEventListener('click', popupToggle);
+popupAddElementClose.addEventListener('click',popupToggle);
