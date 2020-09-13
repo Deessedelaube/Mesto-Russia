ca2c4f10fa23321fe61ecac8b743b1bb3fb149ca
Daria Mishunina <mishuninadaria@gmail.com>
2020-09-13 18:13:14 +0400

    feat: add cards popup done

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
-const popupProfile = document.querySelector('.popup_profile');
-const popupAddElement = document.querySelector('.popup_addElement');
+const popup = document.querySelectorAll('.popup');
+const popupProfile = popup[0];
+const popupAddElement = popup[1];
+console.log(popupProfile,popupAddElement);
 const page = document.querySelector('.page');
 const popupProfileOpenButton = document.querySelector('.button_type_edit');
 const popupAddElementOpenButton = document.querySelector('.button_type_add');
@@ -14,10 +16,66 @@ let jobInput = popupProfile.querySelector('.form__description');
 // Выбираем элементы профиля, куда должны быть вставлены значения полей
 let profileName = document.querySelector('.profile__name');
 let profilejob = document.querySelector('.profile__job');
-
-let formAddElement = popupAddElement.querySelector('form_addElement');
+// Находим форму добавления карточки места
+let formAddElement = popupAddElement.querySelector('.form_addElement');
 let titleInput = popupAddElement.querySelector('.form__fullname');
 let srcInput = popupAddElement.querySelector('.form__description');
+
+let elements = document.querySelector('.elements');
+const itemTemplate = document.querySelector('.element_template').content;
+
+let places = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+},
+{
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+},
+{
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+},
+{
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+},
+{
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+},
+{
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+}
+];
+function render (){
+  elements.innerHTML ="";
+  places.forEach((item) =>{
+    const htmlElement = itemTemplate.cloneNode(true);
+    htmlElement.querySelector('.element__title').innerText = item.name;
+    htmlElement.querySelector('.element__image').src = item.link;
+    elements.appendChild(htmlElement);
+  })
+};
+
+//обработчик формы добавления карточки
+function formAddElementSubmitHandler (){
+debugger;
+  console.log('form');
+const newelement = {};
+newelement.name = titleInput.value;
+newelement.link = srcInput.value;
+  console.log(newelement);
+places.unshift(newelement);
+console.log(places);
+
+render(places);
+titleInput.value ="";
+srcInput.value ="";
+popupToggle();
+};
 
 //функция открытия/закрытия попапа добавления карточки
 const popupToggle = () =>{
@@ -32,7 +90,6 @@ const popupProfileToggle = () => {
     nameInput.value = profileName.textContent;
     jobInput.value = profilejob.textContent;
   }
-  //открываем-закрываем попап
   popupProfile.classList.toggle('popup_opened');
   page.classList.toggle('page_overflow_hidden');
 };
@@ -53,10 +110,13 @@ function formProfileSubmitHandler (evt) {
 }
 
 //Вставляем все слушатели событий
+render();
 popupProfileOpenButton.addEventListener('click', popupProfileToggle);
 formProfile.addEventListener('submit', formProfileSubmitHandler);
 popupProfileClose.addEventListener('click', popupProfileToggle);
 popupProfile.addEventListener('click', popupCloseByClickonOverlay);
 
 popupAddElementOpenButton.addEventListener('click', popupToggle);
+formAddElement.addEventListener('submit', formAddElementSubmitHandler);
 popupAddElementClose.addEventListener('click',popupToggle);
+
