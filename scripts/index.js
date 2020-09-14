6549cdced19c6aefcc457d4049aea7c77eb11bba
Daria Mishunina <mishuninadaria@gmail.com>
2020-09-14 21:24:52 +0400

    feat: popup images/ transition

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const popup = document.querySelectorAll('.popup');
 const popupProfile = popup[0];
 const popupAddElement = popup[1];
-console.log(popupProfile,popupAddElement);
+const popupEnlargeImage = popup[2];
 const page = document.querySelector('.page');
 const popupProfileOpenButton = document.querySelector('.button_type_edit');
 const popupAddElementOpenButton = document.querySelector('.button_type_add');
-// Можно ли найти универсальную кнопку закрытия?
+
 const popupProfileClose = popupProfile.querySelector('.button_type_close');
 const popupAddElementClose = popupAddElement.querySelector('.button_type_close');
 
@@ -21,10 +21,10 @@ let formAddElement = popupAddElement.querySelector('.form_addElement');
 let titleInput = popupAddElement.querySelector('.form__fullname');
 let srcInput = popupAddElement.querySelector('.form__description');
 
-let elements = document.querySelector('.elements');
+const elements = document.querySelector('.elements');
 const itemTemplate = document.querySelector('.element_template').content;
 
-let places = [
+const places = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -50,6 +50,7 @@ let places = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
 }
 ];
+//отрисовщик массива картинок
 function render (){
   elements.innerHTML ="";
   places.forEach((item, index) =>{
@@ -65,19 +66,14 @@ function render (){
 
 //обработчик формы добавления карточки
 function formAddElementSubmitHandler (){
-debugger;
-  console.log('form');
-const newelement = {};
-newelement.name = titleInput.value;
-newelement.link = srcInput.value;
-  console.log(newelement);
-places.unshift(newelement);
-console.log(places);
-
-render(places);
-titleInput.value ="";
-srcInput.value ="";
-popupToggle();
+  const newelement = {};
+    newelement.name = titleInput.value;
+    newelement.link = srcInput.value;
+  places.unshift(newelement);
+  render(places);
+  titleInput.value ="";
+  srcInput.value ="";
+  popupToggle();
 };
 
 //функция открытия/закрытия попапа добавления карточки
@@ -87,13 +83,24 @@ const popupToggle = () =>{
 };
 
 //* функция открытия/закрытия попапа профиля
-const popupProfileToggle = () => {
+const popupProfileToggle = (event) => {
   //если форма была закрыта, то записываем в форму значения полей профиля
   if (!popupProfile.classList.contains('popup_opened')){
     nameInput.value = profileName.textContent;
     jobInput.value = profilejob.textContent;
-  }
+  };
   popupProfile.classList.toggle('popup_opened');
+  page.classList.toggle('page_overflow_hidden');
+};
+
+//функция открытия/закрытия попапа картинок
+const popupEnlargeImageToggle = (event)=>{
+  //если форма была закрыта, то задиваем в нее картинку и название
+  if (!popupEnlargeImage.classList.contains('popup_opened')){
+    popupEnlargeImage.querySelector('.popup__image').setAttribute('src', event.target.src);
+    popupEnlargeImage.querySelector('.popup__title_image').textContent = event.target.alt;
+  };
+  popupEnlargeImage.classList.toggle('popup_opened');
   page.classList.toggle('page_overflow_hidden');
 };
 
@@ -106,7 +113,6 @@ const popupCloseByClickonOverlay = (event) => {
 // Обработчик «отправки» формы
 function formProfileSubmitHandler (evt) {
   evt.preventDefault(); //убираем стандартную отправку
-// Вставляем новые значения
   profileName.textContent = nameInput.value;
   profilejob.textContent = jobInput.value;
   popupProfileToggle();
@@ -114,14 +120,15 @@ function formProfileSubmitHandler (evt) {
 
 //функция удаления карточки
 function handleDelete (event){
-  const index = event.target.parentNode.getAttribute("data-id");
+  let index = event.target.parentNode.getAttribute("data-id");
+  console.log(index);
   places.splice(index, 1);
   render();
 };
 
+//ставим лайки
 function handleLike (event){
   event.target.classList.toggle('button_type_like_clicked');
-
 };
 
 //Вставляем все слушатели событий
@@ -132,6 +139,9 @@ function setListeners (){
   document.querySelectorAll('.button_type_like').forEach((btn)=>{
     btn.addEventListener('click', handleLike);
   });
+  document.querySelectorAll('.button_type_enlarge').forEach((btn)=>{
+    btn.addEventListener('click',popupEnlargeImageToggle);
+  })
 };
 
 popupProfileOpenButton.addEventListener('click', popupProfileToggle);
@@ -143,4 +153,5 @@ popupAddElementOpenButton.addEventListener('click', popupToggle);
 formAddElement.addEventListener('submit', formAddElementSubmitHandler);
 popupAddElementClose.addEventListener('click',popupToggle);
 
+popupEnlargeImage.querySelector('.button_type_close').addEventListener('click',popupEnlargeImageToggle);
 render();
